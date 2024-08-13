fec9031e261c25b8a157d509a2f42131152134f9
HridayNayak25 <hridaynayak8@gmail.com>
2024-08-13 10:12:44 -0700

    Update Program4.java
    Code 4 phase 1

--- Program4.java
+++ Program4.java
@@ -4,7 +4,59 @@
 
 
 //Paste console output below:
+//Youre on your own!
+//Jk ask for help if you need it (but also look at previous programs!!)
+import  java.util.Scanner;
+public class program4{
+    public static void main(String [] args){
+        Scanner myScanner = new Scanner (System.in);
+        
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        int w = 0;
+        int sum = 0;
+        int average = 0;
+        double average_1 = 0;
+        
+        System.out.println("x = ");
+        x = myScanner.nextInt();
+        System.out.println("y = ");
+        y = myScanner.nextInt();
+        System.out.println("z = ");
+        z = myScanner.nextInt();
+        System.out.println("w = ");
+        w = myScanner.nextInt();
+        
+        //calculate
+        sum = (x+y+z+w);
+        average = ((x+y+z+w)/4);
+        average_1 = ((x+y+z+w)/4.0);
+    
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+
+    
+}
+
+
+
+//Paste console output below:
 /*
 
 
 */
+
+/*
+
+
+
+*/
+
